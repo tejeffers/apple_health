2a76802142dfa82db78e4c9680caa6196c8d026f
tejeffers <jeffers.tess@gmail.com>
2020-08-17 22:24:55 -0400

    extract weather from metadata dict

--- src/parse_data.py
+++ src/parse_data.py
@@ -1,21 +1,8 @@
-import pandas as pd
+iport pandas as pd
 import xmltodict
 
 from workout import Workout
-
-COLUMNS = ['activity_type',
-           'duration',
-           'duration_unit',
-           'total_distance',
-           'total_distance_unit',
-           'total_energy_burned',
-           'total_energy_burned_unit',
-           'device',
-           'creation_date',
-           'start_date',
-           'end_date',
-           'route',
-           'metadata_dict']
+from common import * 
 
 
 def parse_xml(path_to_file):
@@ -24,11 +11,55 @@ def parse_xml(path_to_file):
     return doc
 
 
+def parse_metadata(row):
+    if row.get('HKIndoorWorkout', None) is not None:
+        return pd.Series([bool(row.get('HKIndoorWorkout')), 
+                          row.get('HKAverageMETs', ' kcal/hr·kg').split(' kcal/hr·kg')[0],
+                          row.get('HKWeatherTemperature', ' degF').split(' degF')[0],
+                          row.get('HKWeatherHumidity', ' %').split(' %')[0], 
+                          row.get('HKTimeZone'),
+                          row.get('HKElevationAscended', ' cm').split(' cm')[0]])
+    else:
+        return pd.Series([None,None,None,None,None,None])
+    
+    
+def add_metadata_unit(df):
+    df['average_mets_unit'] = 'kcal/hr·kg'
+    df['temperature_unit'] = 'degF'
+    df['humidity_unit'] = '%'
+    df['elevation_ascended_unit'] = 'cm'
+    return df
+
+
+def append_metadata(df):
+    
+    df[['was_indoor_workout','average_mets','temperature',
+        'humidity', 'timezone','elevation_ascended']] = df.metadata_dict.apply(lambda x: parse_metadata(x))
+    
+    return add_metadata_unit(df)
+
+
+def format_columns(df):
+    df = df[FORMATTED_COLUMNS]
+    df = df.replace(r'', None, regex=True)
+
+    for dcol in DATETIMESTAMP_COLUMNS:
+        df[dcol] = pd.to_datetime(df[dcol])
+
+    for fcol in FLOAT_COLUMNS:
+        df[fcol] = df[fcol].astype(float)
+    return df
+
+
+
 def create_workout_df():
     data = parse_xml('..data/export.xml')
     workouts = data['HealthData'].get('Workout')
     series_list = [Workout(i).return_pd_series() for i in workouts]
     workout_df = pd.DataFrame(series_list)
     workout_df.columns = COLUMNS
+    workout_df = append_metadata(workout_df)
+    workout_df = format_columns(workout_df)
+
     return workout_df
 
